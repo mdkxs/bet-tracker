328f55b523faa5446ce2ae13b2aff09109ca1b64
mdkxs <migueljardel10@gmail.com>
2026-02-27 15:39:14 +0000

    fix supabase duplicate

--- app/page.js
+++ app/page.js
@@ -219,12 +219,6 @@ function StatCard({ title, value, highlight }) {
   );
   
 }
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
 
 export default async function Profile({ params }) {
   const { username } = params;
